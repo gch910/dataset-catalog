3c4d80de18afca7d86aa55450bfa2964bde6c148
Gabriel Harris <gch910@gmail.com>
2026-03-22 15:08:38 -0500

    add cors

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,7 +1,20 @@
 using DatasetCatalog.Api.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("Frontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
+
+app.UseCors("Frontend");
 
 app.MapGet("/api/datasets", () =>
 {
